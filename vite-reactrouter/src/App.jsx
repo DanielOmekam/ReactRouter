a93ea8ce5f9a13b33a4fad6cabab914dab70fa95
Daniel Omekam <omekam.daniel@gmail.com>
2024-10-17 14:54:35 -0400

    blue red

--- vite-reactrouter/src/App.jsx
+++ vite-reactrouter/src/App.jsx
@@ -2,20 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Blue from "./Blue";
+import Red from "./Red";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    d
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      
+      <div id="navbar">
+        <Link to ="/blue">Blue</Link>
+        <Link to = "/red">Red</Link>
+
+      </div>
+      <div id="main-section">
+        <Routes>
+        <Route path = "/home" element = {<Home />} />
+          <Route path = "/blue" element = {<Blue />} />
+          <Route path = "/red" element = {<Red />} />
+        </Routes>
+        
+
+      </div>
     </div>
     </>
-  )
-}
+  );
+};
 
 export default App
